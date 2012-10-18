a9033a9f491c907b4cddc4a1d9f5f15bea3de381
André Lubian <lubiana@lubieee.(none)>
2012-10-18 23:39:06 +0200

    A little cosmetic work

--- post.php
+++ post.php
@@ -5,7 +5,7 @@
  ***************************/
 $urlfile = "urls"; 			// file to store the urls
 $title = "Linkpaste Lite";	// Title of the HTML document
-$limit = 15;				// number of links to show ( 0 = no limit)
+$limit = 15;				// number of links to show ( 0 = no limit )
 
 
 
@@ -50,10 +50,8 @@ function createIndex() {
 	}
 	global $title;
 	return("
-<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
-	\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
-<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">
-
+<!DOCTYPE html>
+<html lang=\"en\">
 <head>
 	<title> $title </title>
 	<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\" />
@@ -103,10 +101,8 @@ function getUrls(){
 	return $return;
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
 	<title> <?= $title ?> </title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
