fb7b6ddfe65296606275953b6299c4e23abc668a
lubiana <lubiana123@gmail.com>
2013-07-01 01:25:56 +0200

    some long overdue cosmetic work

--- post.php
+++ post.php
@@ -1,116 +1,116 @@
 <?php
 
-/****************************
- * Configuration:			*	
- ***************************/
-$urlfile = "urls"; 			// file to store the urls
-$title = "Linkpaste Lite";	// Title of the HTML document
-$limit = 15;				// number of links to show ( 0 = no limit )
+  /****************************
+   * Configuration:           *
+   ****************************/
+  $urlfile = "urls";          // file to store the urls
+  $title = "Linkpaste Lite";  // Title of the HTML document
+  $limit = 15;                // number of links to show ( 0 = no limit )
 
 
 
-if($_GET['sub'] == 'OK'){
-		$url = $_GET['link'];
-		$beschreibung = $_GET['beschreibung'];
-		writeUrlFile();
-		writeIndex();
-		unset($_GET);
-		header('location: index.html');
-	}
+  if($_GET['sub'] == 'OK'){
+      $url = $_GET['link'];
+      $beschreibung = $_GET['beschreibung'];
+      writeUrlFile();
+      writeIndex();
+      unset($_GET);
+      header('location: index.html');
+    }
 
 
-/* Functions */
+  /* Functions */
 
-function writeUrlFile(){
-	global  $url, $beschreibung, $urlfile;
-	if(!empty($url)&&!empty($beschreibung)) {		
-		$file = fopen($urlfile, 'a+');
-		$urleins = 'http://';
-		if(!eregi('^http:\/\/',$url)){
-			$urleins .= $url;
-		}
-		else {
-			$urleins = $url;
-		}
-	fwrite($file,"\"$urleins\";\"$beschreibung\"\n");
-	fclose($file);	
-	}
-}
+  function writeUrlFile() {
+    global  $url, $beschreibung, $urlfile;
+    if(!empty($url) && !empty($beschreibung)) {
+      $file = fopen($urlfile, 'a+');
+      $urleins = 'http://';
+      if(!eregi('^http:\/\/',$url)){
+        $urleins .= $url;
+      }
+      else {
+        $urleins = $url;
+      }
+    fwrite($file,"\"$urleins\";\"$beschreibung\"\n");
+    fclose($file);
+    }
+  }
 
-function writeIndex(){
-	$file = fopen("index.html", "w+");
-	fwrite($file, createIndex());
-	fclose($file);
-}
+  function writeIndex(){
+    $file = fopen("index.html", "w+");
+    fwrite($file, createIndex());
+    fclose($file);
+  }
 
-function createIndex() {
-	$links = "";
-	foreach(getUrls() as $value){
-		$links .= "<li class=\"links\">\n\t\t\t<a href=\"". $value[0] ."\">". $value[1]. "</a><br /> " .$value[0]."\n\t\t</li>\n\t\t"; 
-	}
-	global $title;
-	return("
-<!DOCTYPE html>
-<html lang=\"en\">
-<head>
-	<title> $title </title>
-	<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\" />
-	<meta name=\"generator\" content=\"Linkpaste\" />
-	<link href=\"style.css\" rel=\"stylesheet\" type=\"text/css\">
-</head>
+  function createIndex() {
+    $links = "";
+    foreach(getUrls() as $value){
+      $links .= "<li class=\"links\">\n\t\t\t<a href=\"". $value[0] ."\">". $value[1]. "</a><br /> " .$value[0]."\n\t\t</li>\n\t\t"; 
+    }
+    global $title;
+    return("
+  <!DOCTYPE html>
+  <html lang=\"en\">
+  <head>
+    <title> $title </title>
+    <meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\" />
+    <meta name=\"generator\" content=\"Linkpaste\" />
+    <link href=\"style.css\" rel=\"stylesheet\" type=\"text/css\">
+  </head>
 
-<body>
-	<form action=\"post.php\" method=\"GET\">
-		<ul id=\"form\">
-			<li>
-				<label>
-					<span>Url: </span>
-					<input type=\"text\" name=\"link\">
-				</label>
-			</li>
-			<li>
-				<label>
-					<span>Beschreibung: </span>
-					<input type=\"text\" name=\"beschreibung\">
-				</label>
-			</li>
-			<li>
-				<input type=\"submit\" name=\"sub\" value=\"OK\">
-			</li>
-		</ul>
-	</form>
-	<ul id=\"wrapper\">
-		$links
-	</ul>
-</body>
+  <body>
+    <form action=\"post.php\" method=\"GET\">
+      <ul id=\"form\">
+        <li>
+          <label>
+            <span>Url: </span>
+            <input type=\"text\" name=\"link\">
+          </label>
+        </li>
+        <li>
+          <label>
+            <span>Beschreibung: </span>
+            <input type=\"text\" name=\"beschreibung\">
+          </label>
+        </li>
+        <li>
+          <input type=\"submit\" name=\"sub\" value=\"OK\">
+        </li>
+      </ul>
+    </form>
+    <ul id=\"wrapper\">
+      $links
+    </ul>
+  </body>
 
-</html>	
-	");
-}
+  </html> 
+    ");
+  }
 
-function getUrls(){
-	global $urlfile, $limit;
-	$return = array();
-	$data = file($urlfile,	FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-	
-	if(count($data) > $limit && $limit > 0)
-		$data = array_slice($data, count($data) - $limit);
-	foreach($data as $value)
-		$return[] = explode("\";\"", trim($value, "\""), 3);
-	}	
-	return $return;
-}
+  function getUrls(){
+    global $urlfile, $limit;
+    $return = array();
+    $data = file($urlfile,  FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    if(count($data) > $limit && $limit > 0)
+      $data = array_slice($data, count($data) - $limit);
+    foreach($data as $value) {
+      $return[] = explode("\";\"", trim($value, "\""), 3);
+    }
+    return $return;
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title> <?= $title ?> </title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-	<meta name="generator" content="Linkpaste" />
-	<link href="style.css" rel="stylesheet" type="text/css">
+  <title> <?= $title ?> </title>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <meta name="generator" content="Linkpaste" />
+  <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-	<h1> Fehlerhafte Eingabe </h1>
-	<p>Nochmal <a href="index.html">versuchen?</a><p>
+  <h1> Fehlerhafte Eingabe </h1>
+  <p>Nochmal <a href="index.html">versuchen?</a><p>
 </body>
 </html>
